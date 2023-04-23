6a2b2b665992e8ef367e5348b2ef0a2495967787
Rohit-Nakhate <rohit.nakhate@a-devops>
2023-04-23 13:15:45 +0530

    add and sub fun

---
+++ Test.py
@@ -0,0 +1,19 @@
+
+
+def add (a,b):
+    sum = a+b
+    print(sum)
+    return a,b
+
+def sub (c,d):
+    sub = c-d
+    print(sub)
+    return c,d
+
+def main():
+    add(110,20)
+    sub(80,10)
+
+
+if __name__=='__main__':
+    main()
